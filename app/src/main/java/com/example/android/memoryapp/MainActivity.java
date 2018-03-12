5e8eaa27f6e8736e7e635495045cc9e428c59605
Lucian Epure <lucian.epure@yahoo.com>
2018-03-12 22:39:21 +0200

    Save memory GUI

--- app/src/main/java/com/example/android/memoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/memoryapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.android.memoryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button saveMemory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        saveMemory = findViewById(R.id.saveMemory);
+        saveMemory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, PhotoSel.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
